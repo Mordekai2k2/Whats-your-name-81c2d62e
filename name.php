cc40091f5b62afae0117ff99b9b96739430c5bed
Randy Brouwer <2105070@talnet.nl>
2019-09-20 11:37:46 +0100

    Uitwerking van de exercise

---
+++ name.php
@@ -0,0 +1,10 @@
+<?php
+echo "Hewwo my name is Mumfwr!! Who is you OwO???   ";
+$text = readline();
+echo "Wand wuts your last name OwO?   ";
+$last_name = readline();
+echo "Your awnser is ";
+echo $text ," ", $last_name;
+echo ", Big UwU!!";
+echo " Gimme your buldgy wuldgy!!";
+
